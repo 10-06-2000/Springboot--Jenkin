50c60593a566fa09380c1c769c92aec343154d7f
Athulya Vijayan <athulyavijayan20@gmail.com>
2023-05-22 12:31:04 +0530

    second

---
+++ src/main/java/com/example/demo/Controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DemoController {
+    @GetMapping("/welcome")
+    public String welcom() {
+        return "welcome";
+    }
+    @GetMapping("/login")
+    public String log(){
+        return "login";
+    }
+
+
+}
+
+
